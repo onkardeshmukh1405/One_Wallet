87ca0a83da17e0059ee560ae7f2a30a8902f54fa
onkardeshmukh1405 <onkardeshmukh667@gmail.com>
2024-08-09 16:26:07 +0530

    build cmd

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 require("dotenv").config();
 
@@ -15,6 +16,23 @@ const requestsRoute = require("./routes/requestsRoute");
 app.use("/api/users", usersRoute);
 app.use("/api/transactions", transactionsRoute);
 app.use("/api/requests", requestsRoute);
+
+// -----------------Deployment--------------
+
+const _dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname1, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running Successfully");
+  });
+}
+
+// -----------------Deployment--------------//
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
